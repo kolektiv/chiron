5d88fc32af7fc30bdcadba0fb614d220d99ff6f8
kolektiv <andrew@xyncro.com>
2016-01-09 02:10:59 +0000

    attempting some benchmarks

---
+++ tests/Chiron.Benchmarks/Program.fs
@@ -0,0 +1,66 @@
+﻿module Chiron.Benchmarks
+
+open BenchmarkDotNet
+open BenchmarkDotNet.Tasks
+open Chiron
+open Chiron.Operators
+open Newtonsoft.Json
+
+(* Examples *)
+
+type SimpleRecord =
+    { Id: int
+      Name: string
+      Address: string
+      Scores: int array }
+
+    static member ToJson (x: SimpleRecord) =
+            Json.write "id" x.Id
+         *> Json.write "name" x.Name
+         *> Json.write "address" x.Address
+         *> Json.write "scores" x.Scores
+
+    static member FromJson (_: SimpleRecord) =
+            fun id name address scores ->
+                { Id = id
+                  Name = name
+                  Address = address
+                  Scores = scores }
+        <!> Json.read "id"
+        <*> Json.read "name"
+        <*> Json.read "address"
+        <*> Json.read "scores"
+
+(* Benchmarks *)
+
+[<BenchmarkTask (platform = BenchmarkPlatform.X86,
+                 jitVersion = BenchmarkJitVersion.LegacyJit, 
+                 mode = BenchmarkMode.SingleRun,
+                 processCount = 1,
+                 warmupIterationCount = 1,
+                 targetIterationCount = 1)>]
+type ChironVsJsonNet () =
+
+    let simpleRecord =
+        { Id = 42
+          Name = "Andrew"
+          Address = "Brighton"
+          Scores = [| 12; 20; 45 |] }
+
+    [<Benchmark>]
+    member __.ChironSerialize () =
+        (Json.serialize >> Json.format) simpleRecord
+
+    [<Benchmark>]
+    member __.JsonNetSerialize () =
+        JsonConvert.SerializeObject simpleRecord
+
+(* Main *)
+
+[<EntryPoint>]
+let main _ =
+
+    let runner = new BenchmarkRunner ()
+    let reports = runner.Run<ChironVsJsonNet> ()
+
+    0
